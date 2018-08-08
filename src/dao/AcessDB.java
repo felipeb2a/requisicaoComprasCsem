2503a65217a3a476c6ab63a3df26340d013e8596
felipeb2a <felipe.m.ferreira@hotmail.com>
2018-08-08 17:17:37 -0300

    Revisão mysql 8.0 - 08-08-18
    Adicionar parametros de SSL e TIMEZONE na conexao com o bando de dados.

--- src/dao/AcessDB.java
+++ src/dao/AcessDB.java
@@ -10,28 +10,35 @@ public class AcessDB {
     protected Connection conectar(String nameDb) throws ClassNotFoundException, SQLException {
         Class.forName //MYSQL
                 ("com.mysql.jdbc.Driver");
-        
-        //PRINCIPAL 
-        
+        /*
+        //PRINCIPAL         
         String endereco = "15.0.0.9";        
         String database = nameDb;
+        //MYSQL 8.0 REQUER SSL E TIMEZONE
+        //String ssl = "?useSSL=false";
+        //String timeZone = "&useTimezone=true&serverTimezone=UTC";
         String usuario = "compras";
         String senha = "Compr@s2017";
         String url = "jdbc:mysql://" + endereco + "/" + database;
 
         return DriverManager.getConnection(url, usuario, senha);        
 /*######################################################################################*/
-        /*
-        //TESTE        
-        String endereco = "15.0.9.184";        
-        //String endereco = "localhost";  
-        String database = nameDb;
-        String usuario = "root";
-        String senha = "root";
-        String url = "jdbc:mysql://" + endereco + "/" + database;
         
+        //TESTE        
+        //String endereco = "15.0.9.184";        
+        String endereco = "15.0.0.8";
+        String database = nameDb;
+        //MYSQL 8.0 REQUER SSL E TIMEZONE
+        String ssl = "?useSSL=false";
+        String timeZone = "&useTimezone=true&serverTimezone=UTC";
+        //String usuario = "root";
+        //String senha = "root";
+        String usuario = "compras";
+        String senha = "Compr@s2017";
+        String url = "jdbc:mysql://" + endereco + "/" + database + ssl + timeZone;
+
         return DriverManager.getConnection(url, usuario, senha);
-/*######################################################################################*/        
+        /*######################################################################################*/
     }
 
 }
