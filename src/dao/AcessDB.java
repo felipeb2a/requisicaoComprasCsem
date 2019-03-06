b40bd41b49ed1562b76bd9fcec5b360142d16aca
felipeb2a <felipe.m.ferreira@hotmail.com>
2019-03-06 14:24:23 -0300

    Configurar POOl de conexoes
    Configurar POOl de conexoes com o DBCP apache.

--- src/dao/AcessDB.java
+++ src/dao/AcessDB.java
@@ -4,13 +4,45 @@ package dao;
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
+import org.apache.commons.dbcp.BasicDataSource;
 
 public class AcessDB {
 
     protected Connection conectar(String nameDb) throws ClassNotFoundException, SQLException {
-        Class.forName //MYSQL
-                ("com.mysql.jdbc.Driver");
         /*
+        //PRINCIPAL POOL CONECTIONs      
+        //PROPRIEDADES DE CONEXAO DO BANCO       
+        String endereco = "15.0.0.9";
+        String database = nameDb;
+        //MYSQL 8.0 REQUER SSL E TIMEZONE
+        String ssl = "?useSSL=false";
+        String timeZone = "&useTimezone=true&serverTimezone=UTC";
+        
+        String urlSSL = "jdbc:mysql://" + endereco + "/" + database + ssl + timeZone;
+        String url = "jdbc:mysql://" + endereco + "/" + database;
+        //USUARIO DE ACESSO AO BANCO
+        String usuario = "compras";
+        String senha = "Compr@s2017";
+
+        BasicDataSource dataSource = new BasicDataSource();
+        dataSource.setDriverClassName("com.mysql.jdbc.Driver");
+        dataSource.setUrl(urlSSL);
+        dataSource.setUsername(usuario);
+        dataSource.setPassword(senha);
+        
+        dataSource.setInitialSize(1);
+        dataSource.setMinIdle(5);
+        dataSource.setMaxIdle(20);
+        dataSource.setMaxOpenPreparedStatements(200);
+        
+        Connection con = dataSource.getConnection();
+        return con;
+        
+        /*######################################################################################*/
+        /*
+        Class.forName //MYSQL
+                ("com.mysql.jdbc.Driver");        
+        
         //PRINCIPAL       
         
         String endereco = "15.0.0.9";        
@@ -24,7 +56,7 @@ public class AcessDB {
 
         return DriverManager.getConnection(url, usuario, senha);        
 /*######################################################################################*/
-        
+        /*
         //TESTE        
         //String endereco = "15.0.9.184";        
         String endereco = "15.0.0.8";
@@ -34,10 +66,40 @@ public class AcessDB {
         String timeZone = "&useTimezone=true&serverTimezone=UTC";
         String usuario = "root";
         String senha = "root";
-        //String url = "jdbc:mysql://" + endereco + "/" + database + ssl + timeZone;
+        //String urcl = "jdbc:mysql://" + endereco + "/" + database + ssl + timeZone;
         String url = "jdbc:mysql://" + endereco + "/" + database;
         //System.out.println(url);
         return DriverManager.getConnection(url, usuario, senha);
+        /*######################################################################################*/
+        
+        //TESTE POOL CONECTIONs      
+        //PROPRIEDADES DE CONEXAO DO BANCO
+        //String endereco = "15.0.9.184";        
+        String endereco = "15.0.0.8";
+        String database = nameDb;
+        //MYSQL 8.0 REQUER SSL E TIMEZONE
+        String ssl = "?useSSL=false";
+        String timeZone = "&useTimezone=true&serverTimezone=UTC";
+        
+        String urlSSL = "jdbc:mysql://" + endereco + "/" + database + ssl + timeZone;
+        String url = "jdbc:mysql://" + endereco + "/" + database;
+        //USUARIO DE ACESSO AO BANCO
+        String usuario = "root";
+        String senha = "root";
+
+        BasicDataSource dataSource = new BasicDataSource();
+        dataSource.setDriverClassName("com.mysql.jdbc.Driver");
+        dataSource.setUrl(urlSSL);
+        dataSource.setUsername(usuario);
+        dataSource.setPassword(senha);
+        
+        dataSource.setInitialSize(1);
+        dataSource.setMinIdle(5);
+        dataSource.setMaxIdle(20);
+        dataSource.setMaxOpenPreparedStatements(200);
+        
+        Connection con = dataSource.getConnection();
+        return con;
         /*######################################################################################*/
     }
 
